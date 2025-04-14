9b23d78f5d3214ad102529f32692519889be118e
oahussein5 <omar.hussein@ndsu.edu>
2025-04-14 10:35:13 -0500

    G C for forms and created data service and forms interface.

---
+++ FarPhones/src/app/form-edit/form-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-edit',
+  imports: [],
+  templateUrl: './form-edit.component.html',
+  styleUrl: './form-edit.component.css'
+})
+export class FormEditComponent {
+
+}
