7fad85f7593fb53cbd475529f5d0f52e86267e6f
miketoq <michael.toquam@ndus.edu>
2025-04-21 19:45:32 -0500

    First part of the home-card

--- FarPhones/src/app/app.routes.ts
+++ FarPhones/src/app/app.routes.ts
@@ -1,3 +1,46 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { FormAddComponent } from './form-add/form-add.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { FormEditComponent } from './form-edit/form-edit.component';
+import { FormListComponent } from './form-list/form-list.component';
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: "",
+        component: HomeComponent,
+        title: "Home"
+    },
+
+    {
+        path: "directory",
+        component: FormListComponent,
+        title: "Directory List"
+    },
+
+    // {
+    //     path: "forms",
+    //     component: 
+    // },
+
+    {
+        path: "about",
+        component: AboutUsComponent,
+        title: "About Us"
+    },
+
+    {
+        path: "add",
+        component: FormAddComponent,
+        title: "Add Form"
+    },
+
+    {
+        path: "contact/:id",
+        component: FormEditComponent,
+        title: "Edit Form"
+    }
+];
