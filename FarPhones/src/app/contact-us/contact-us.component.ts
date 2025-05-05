2fdf9a04ae8243bfdbb87b5fe5b478d2ac0507c2
oahussein5 <omar.hussein@ndsu.edu>
2025-05-05 16:55:00 -0500

    Contact Us Component

--- FarPhones/src/app/contact-us/contact-us.component.ts
+++ FarPhones/src/app/contact-us/contact-us.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.css'
+  styleUrl: './contact-us.component.css',
 })
-export class ContactUsComponent {
-
-}
+export class ContactUsComponent {}
